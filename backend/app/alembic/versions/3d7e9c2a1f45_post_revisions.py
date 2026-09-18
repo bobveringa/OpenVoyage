a81901ae0964d815e2c1981d257ee3ddef8ad321
Bob Veringa <github@alias-mail.bobbaletta.nl>
2026-09-18 16:52:34 +0200

    Fix issue with migrations

--- backend/app/alembic/versions/3d7e9c2a1f45_post_revisions.py
+++ backend/app/alembic/versions/3d7e9c2a1f45_post_revisions.py
@@ -1,7 +1,7 @@
 """add optimistic-concurrency revisions to posts
 
 Revision ID: 3d7e9c2a1f45
-Revises: f18c2a7d9b31, 4a9d2e7b1c53
+Revises: 4a9d2e7b1c53
 Create Date: 2026-09-18 00:00:00.000000+00:00
 
 """
@@ -13,10 +13,7 @@ import sqlalchemy as sa
 
 
 revision: str = '3d7e9c2a1f45'
-down_revision: Union[str, Sequence[str], None] = (
-    'f18c2a7d9b31',
-    '4a9d2e7b1c53',
-)
+down_revision: Union[str, Sequence[str], None] = '4a9d2e7b1c53'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
